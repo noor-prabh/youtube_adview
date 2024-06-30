fd145954ad6c3eb7b8dea9c3b5a3579ed73f4fc4
prabhnoor1301 <prabh13noorkaur@gmail.com>
2024-06-30 16:30:58 +0530

    completion of project

--- main.py
+++ main.py
@@ -100,3 +100,101 @@ sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool)
 plt.show()
               
 #Splitting dataset
+y_train = pd.DataFrame(data=data_train.iloc[:,1].values, columns=['target'])
+data_train = data_train.drop(["adview"], axis=1)
+data_train = data_train.drop(["vidid"], axis=1)
+data_train.head()
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(data_train, y_train, test_size=0.2, random_state=2)
+x_train.shape
+
+#normalize data
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
+
+x_train.mean()
+
+#evaluation metrics
+from sklearn import metrics
+def print_error(x_test, x_train, model_name):
+    prediction = model_name.predict(x_test)
+    print('Mean absolute error :', metrics.mean_absolute_error(y_test, prediction))
+    print('Mean squared error :', metrics.mean_squared_error(y_test, prediction))
+    print('Root mean squared error :', np.sqrt(metrics.mean_squared_error(y_test, prediction))
+          )
+
+#linear regression
+from sklearn import linear_model
+linear_regression = linear_model.LinearRegression()
+linear_regression.fit(x_train, y_train)
+print_error(x_test, y_test, linear_regression)
+#Mean absolute error : 3101.313562824354
+#Mean squared error : 258641479.6389894
+#Root mean squared error : 16082.334396442246
+
+#decision tree regressor
+from sklearn.tree import DecisionTreeRegressor
+decision_tree = DecisionTreeRegressor()
+decision_tree.fit(x_train, y_train)
+print_error(x_test, y_test, decision_tree)
+#Mean absolute error : 2398.600068306011
+#Mean squared error : 494257637.80635244
+#root mean squared error : 22231.90585186867
+
+#random forest regressor
+from sklearn.ensemble import RandomForestRegressor
+n_estimators = 200
+max_depth = 25
+min_samples_split = 15
+min_samples_leaf = 2
+random_forest = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf)
+random_forest.fit(x_train, y_train)
+print_error(x_test, y_test, random_forest)
+#Mean absolute error : 2586.257158331853
+#Mean squared error : 259690141.1950881
+#Root mean squared error : 16114.904318521041
+
+#support vector regressor
+from sklearn.svm import SVR
+supportvector_regressor = SVR()
+y_train = y_train.values.ravel()
+supportvector_regressor.fit(x_train, y_train)
+print_error(x_test, y_test, supportvector_regressor )
+#Mean absolute error : 1435.6157776650953
+#Mean squared error : 260643200.93903348
+#Root mean squared error : 16144.44799115267
+
+#Artificial neural network
+import keras
+from keras.layers import Dense
+ann = keras.models.Sequential([
+                               Dense(6, activation="relu",
+                               input_shape=x_train.shape[1:]),
+                               Dense(6,activation="relu"),
+                               Dense(1)           
+                               ])
+
+optimizer = keras.optimizers.Adam()
+loss = keras.losses.mean_squared_error
+ann.compile(optimizer=optimizer, loss=loss, metrics=["mean_squared_error"])
+history = ann.fit(x_train, y_train, epochs =100)
+ann.summary()
+print_error(x_test, y_test, ann)
+
+#saving scikitlearn models
+import joblib
+joblib.dump(decision_tree, "decisiontree_youtubeadview.pkl")
+
+#saving keras model
+ann.save('ann_youtubeadview.keras')
+
+
+
+
+
+
+
+
